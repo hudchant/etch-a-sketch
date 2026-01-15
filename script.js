08277052d4a28ca03ce192de66ce92e7b706e39e
hudchant <hudchant@gmail.com>
2026-01-14 19:29:51 -0800

    Create functions that enable grid resizing

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const allBoxes = document.querySelectorAll('.box');
 
     allBoxes.forEach(box => {
-
+        // Set square color to black when mouse enters
         box.addEventListener('mouseenter', () => {
             box.style.backgroundColor = 'black';
         });
-
+        // Set square color to red when mouse exits
         box.addEventListener('mouseout', () => {
             box.style.backgroundColor = 'red';
         });
@@ -17,26 +17,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Listen for button clicks
 document.addEventListener('DOMContentLoaded', function () {
+    // Reference to the button element
     const button = document.querySelector('button');
     button.addEventListener('click', () => {
+        // Prompt user to enter the number of squares per side
         const newSize = prompt('Enter the number of squares per side.');
-
-        if (newSize > 100) {
+        // User can enter any positive integer not exceeding 100
+        while (newSize > 100) {
+            // Ask user to re-enter number if value is invalid
             prompt('Number must not exceed 100. Please try again.');
-        } else {
-            resizeGrid();
+            return; // Exit the function
         }
-
+        // Call the resizeGrid function when user enters a valid value
+        resizeGrid(newSize);
     });
 });
 
 // Function to resize the grid
-function resizeGrid() {
-    boxWidth = (1000 / newSize) - 2;
-    boxHeight = (1000 / newSize) - 2;
+function resizeGrid(newSize) {
 
-    allBoxes.foreach(box => {
-        box.remove();
-    });
+    // Reference to the container element
+    const container = document.querySelector('.container');
+
+    while (container.firstChild) {
+        // Remove the existing 16 x 16 grid
+        container.removeChild(container.firstChild);
+    }
+
+    // Set the width of each new div
+    const boxWidth = (1000 / newSize) - 2;
+    // Loop until the correct number of divs is created based on user input
+    for (let i = 0; i < newSize; i++) {
+        for (let j = 0; j < newSize; j++) {
+            const newBox = document.createElement('div');
+            newBox.classList.add('newBox');
+            newBox.style.width = boxWidth + 'px';
+            newBox.style.height = boxWidth + 'px';
+
+            // Set square color to black when mouse enters
+            newBox.addEventListener('mouseenter', () => {
+                newBox.style.backgroundColor = 'black';
+            });
+            // Set square color to red when mouse exits
+            newBox.addEventListener('mouseout', () => {
+                newBox.style.backgroundColor = 'red';
+            });
+            // Append the new divs to the container element
+            container.appendChild(newBox);
+        }
+    }
 }
 
